096e1a8375b465a297a8e7784831a70a2b940ac7
toufeeqmir <Toufeeqmir124@gmail.com>
2026-06-18 11:07:47 +0530

    project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="bg-red-500 text-white text-6xl font-bold p-10">
+      Tailwind Test
+    </div>
+  );
+}
+
+export default App;
